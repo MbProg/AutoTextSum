2d8cc18c66a0f3656d5c5f5fc88d4a2451a73c8c
Tim <Kronsbein>
2018-06-20 15:41:51 +0200

    data iterator

--- nugget_classifier.py
+++ nugget_classifier.py
@@ -10,7 +10,7 @@ import os
 import time
 import pickle
 from itertools import chain
-import h5py
+import ast
 
 import keras
 from keras.layers import Input, Dense, GRU, Embedding, LSTM, Dense, Dropout, Flatten, Bidirectional
@@ -73,17 +73,23 @@ class Nugget_Classifier():
                   metrics=['mae'])
         return model
 
-    def pickle_generator(self, pathx = 'Data\Xtrain', pathy='Data/Ytrain', path_sent='Data/SentEmbeddings'):
-        with open('Data/Xtrain', 'rb') as fx, open('Data/Ytrain', 'rb') as fy, \
-                open('Data/nugget_candidates', 'rb') as fn, \
-                open('Data/SentEmbeddings', 'rb') as fs, \
-                open('Data/queries', 'rb') as fq, \
-                open('Data/query_sent_embeddings', 'rb') as fqs:
+    def data_iterator(self, pathx ='Data\Xtrain', pathy='Data/Ytrain', path_sent='Data/SentEmbeddings'):
+        i=0
+        encoding='latin-1'
+        while True:
             try:
-                while True:
-                    yield pickle.load(fx), pickle.load(fy), pickle.load(fs), pickle.load(fn), pickle.load(fq)
-            except EOFError:
+                with open('Data/word_sequence/x_{}.npy'.format(i), 'rb',) as f_words,\
+                    open('Data/labels/y_{}.npy'.format(i), 'rb',) as f_labels,\
+                    open('Data/queries/query_{}'.format(i), 'r+', encoding=encoding) as f_queries,\
+                    open('Data/nuggets/nuggets_{}'.format(i), 'r+', encoding=encoding) as f_nuggets:
+                    word_sequence = list(np.load(f_words))
+                    labels = np.load(f_labels)
+                    queries = ast.literal_eval(f_queries.read())
+                    nuggets = ast.literal_eval(f_nuggets.read())
+                    yield word_sequence, labels, queries, nuggets
+            except FileNotFoundError:
                 raise StopIteration
+            i+=1
 
     def preprocess(self, batch_size = 64, num_batches = np.inf):
 
@@ -91,7 +97,6 @@ class Nugget_Classifier():
         i=0
         feature_builder = SimpleFeatureBuilder(self.reader, batch_size=batch_size, limit_embeddings=0)
         gen = feature_builder.generate_sequence_word_embeddings(max_len=8, seed=1)
-        #append mode!
         first_write = True
         while True:
             #todo hdf5 file metadata
@@ -106,9 +111,9 @@ class Nugget_Classifier():
                 break
 
 
-            nuggets_sentence_embeddings = feature_builder.generate_sentence_embeddings(nuggets, tokenized=True)
-            assert nuggets_sentence_embeddings.shape == (batch_size, 512), 'sentence embeddings are shape: {} \n ' \
-                                                                       'for Embeddings of {}'.format(nuggets_sentence_embeddings.shape, nuggets)
+            # nuggets_sentence_embeddings = feature_builder.generate_sentence_embeddings(nuggets, tokenized=True)
+            # assert nuggets_sentence_embeddings.shape == (batch_size, 512), 'sentence embeddings are shape: {} \n ' \
+            #                                                            'for Embeddings of {}'.format(nuggets_sentence_embeddings.shape, nuggets)
 
             # append to each example
             # x = [embedded_seq + sentence_embeddings[i] for i, embedded_seq in enumerate(x)]
@@ -117,7 +122,7 @@ class Nugget_Classifier():
             print(y.shape,y.dtype)
             np.save('Data/word_sequence/x_{}'.format(i), x)
             np.save('Data/labels/y_{}'.format(i), y)
-            np.save('Data/sent_embedding/sent_{}'.format(i), nuggets_sentence_embeddings)
+            # np.save('Data/sent_embedding/sent_{}'.format(i), nuggets_sentence_embeddings)
             f = open('Data/queries/query_{}'.format(i), 'w')
             f.write(repr(queries))
             f.close()
@@ -133,8 +138,7 @@ if __name__ == '__main__':
     n = Nugget_Classifier()
     n.preprocess(batch_size,)
     #train
-    '''
-    batch_generator = n.pickle_generator()
+    batch_generator = n.data_iterator()
     #batch_words, batch_y, batch_sent, nugget, queries = next(batch_generator)
     #
     # model_words = keras.Sequential()
@@ -175,7 +179,7 @@ if __name__ == '__main__':
     # print('Result: {} {}'.format(names, res))
     # print(model_sent.predict(sentence_embeddings[0]), '\nTrue: {}'.format(Ytrain[0]))
     ### Testing the combined model with random Data
-    x_batch, y_batch, sent_batch, nuggets, queries = next(batch_generator)
+    x_batch, y_batch, nuggets, queries = next(batch_generator)
     test_sentences = np.random.randn(64, 15, 300)
     test_query = np.random.randn(64, 4, 300)
     sentence_embedding = np.random.randn(64, 512)
@@ -184,4 +188,3 @@ if __name__ == '__main__':
     X = [np.array(x_batch), test_query, sentence_embedding]
     n.model.train_on_batch(X, y_batch)
     print(n.model.predict(X))
-    '''
